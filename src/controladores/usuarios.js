6a97843bd8c1df5fe797d0f004d5bf829ee6b551
Veronica Bierhals <veronicabierhals1@gmail.com>
2023-11-26 09:23:58 -0300

    criada função editarUsuario

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -29,7 +29,38 @@ const detalharUsuario= async(req,res) =>{
     return res.status(200).json(req.usuario);
 }
 const editarUsuario= async(req,res) =>{
+    const { nome, email, senha } = req.body;
+    const { id } = req.usuario;
 
+    try {
+      const usuarioExiste = await knex('usuarios').where({ id }).first()
+
+      if (!usuarioExiste) {
+        return res.status(404).json({ mensagem: 'Usuario não encontrado' });
+      }
+
+      const senhaCriptografada = await bcrypt.hash(senha, 10)
+
+      if (email !== req.usuario.email) {
+        const emailUsuarioExiste = await knex('usuarios')
+          .where({ email })
+          .first()
+
+        if (emailUsuarioExiste) {
+          return res.status(400).json({ mensagem: 'O e-mail já existe.' });
+        }
+      }
+
+      await knex('usuarios').where({ id }).update({
+        nome,
+        email,
+        senha: senhaCriptografada,
+      })
+      return res.status(204).send();
+      
+    } catch (error) {
+      return res.status(500).json({ mensagem: 'Erro interno do Servidor!' });
+    }
 }
 
 module.exports={
