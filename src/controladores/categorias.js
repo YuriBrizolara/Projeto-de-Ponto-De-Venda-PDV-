fa904eae9b4f39937cc5134bf985894594f62ec7
Nyrioxx <felipe_hlima@hotmail.com>
2023-11-27 15:11:20 -0300

    Correção erro de segurança SQL e refatoração

--- src/controladores/categorias.js
+++ src/controladores/categorias.js
@@ -1,11 +1,14 @@
+const knex = require('knex');
+
 const listarCategorias = async (req, res) => {
     try {
-        const { rows } = await pool.query('select * from categorias')
+        const categorias = await knex('categorias').select('*');
 
-        return res.status(200).json(rows)
+        return res.status(200).json(categorias);
     } catch (error) {
-        return res.status(500).json({ mensagem: 'Erro interno do servidor' })
+        console.error(error);
+        return res.status(500).json({ mensagem: 'Erro interno do servidor' });
     }
+};
 
-}
-module.exports = listarCategorias
+module.exports = listarCategorias;
