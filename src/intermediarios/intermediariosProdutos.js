de086a6e57c7036767e591ad62fd0e8538d0dc08
Veronica Bierhals <veronicabierhals1@gmail.com>
2023-12-02 16:11:49 -0300

    criado intermediario encontrarProduto

--- src/intermediarios/intermediariosProdutos.js
+++ src/intermediarios/intermediariosProdutos.js
@@ -1,24 +1,44 @@
-const knex = require("../conexão");
+const knex = require('../conexão');
 
 const validarCamposProduto = (schema) => async (req, res, next) => {
     const { categoria_id } = req.body;
     try {
         await schema.validateAsync(req.body);
-        const idExiste = await knex('categorias').where({ id: categoria_id }).first();
+        const idExiste = await knex('categorias')
+            .where({ id: categoria_id })
+            .first();
         if (!idExiste) {
-            return res.status(400).json({ 'mensagem': 'A categoria informada é invalida!' });
+            return res
+                .status(400)
+                .json({ mensagem: 'A categoria informada é invalida!' });
         }
 
         next();
-
     } catch (error) {
         return res.status(400).json({
-            mensagem: error.message
-        })
-
+            mensagem: error.message,
+        });
     }
+};
 
+const encontrarProduto = async (req, res, next) => {
+    const { id } = req.params;
 
-}
+    try {
+        const produtoEncontrado = await knex('produtos')
+            .select('*')
+            .where('id', id)
+            .first();
+        if (!produtoEncontrado) {
+            return res.status(404).json({ mensagem: 'Produto não encontrado' });
+        }
 
-module.exports = { validarCamposProduto };
+        next()
+    } catch (error) {
+        return res.status(400).json({
+            mensagem: error.message,
+        });
+    }
+};
+
+module.exports = { validarCamposProduto, encontrarProduto };
