b8748fa46b873059dc7cb76ecdca8b363fdb3e82
CaioKWY <caiokaway@gmail.com>
2023-12-03 11:43:36 -0300

    refatoracao rota de atualizacao

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -88,24 +88,20 @@ const editarProduto = async (req, res) => {
     const { descricao, quantidade_estoque, valor, categoria_id } = req.body
 
     try {
-        const temProduto = await knex('produtos').select('*').where({ id }).first();
 
-        if (!temProduto) {
-            return res.status(404).json({ mensagem: 'Produto não encontrado' });
-        };
+        await encontrarProduto(req, res);
 
-    await knex('produtos').where({ id }).update({
-        descricao,
-        quantidade_estoque,
-        valor,
-        categoria_id
-    });
-     } catch (error) {
+        const produtoAtualizado = await knex('produtos').where({ id }).update({
+            descricao,
+            quantidade_estoque,
+            valor,
+            categoria_id
+        }).returning('*');
+
+        return res.status(200).json({ produtoAtualizado });
+    } catch (error) {
         return res.status(500).json({ mensagem: 'Erro interno do servidor' });
     }
-
-
-    return res.status(204).send();
 };
 
 module.exports = {
