1a166d8b63e727e51c21e3e853a2870f789d440b
Veronica Bierhals <veronicabierhals1@gmail.com>
2023-12-04 12:22:46 -0300

    correção de erros de validação em quantidade_estoque, valor e categoria_id

--- src/validacoes/schemaProduto.js
+++ src/validacoes/schemaProduto.js
@@ -6,20 +6,20 @@ const schemaProduto = joi.object({
         'string.base': 'O campo descrição é obrigatório',
         'string.empty': 'O campo descrição é obrigatório',
     }),
-    quantidade_estoque: joi.number().required().integer().messages({
+    quantidade_estoque: joi.number().required().integer().positive().messages({
         'any.required': 'O campo quantidade estoque é obrigatório',
         'number.base': 'O campo quantidade estoque é obrigatório',
         'number.integer':
             'Insira um numero valido',
     }),
 
-    valor: joi.number().required().integer().messages({
+    valor: joi.number().required().integer().positive().messages({
         'any.required': 'O campo valor é obrigatório',
         'number.base': 'O campo valor é obrigatório',
         'number.integer':
             'Insira um valor em centavos e sem vírgula',
     }),
-    categoria_id: joi.number().required().integer().messages({
+    categoria_id: joi.number().required().integer().positive().messages({
         'any.required': 'O campo Id da categoria é obrigatório',
         'number.base': 'O campo Id da categoria é obrigatório',
         'number.integer':
