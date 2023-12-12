e903b858d19ec4776c4370a46d8570e8513c054b
Nyrioxx <felipe_hlima@hotmail.com>
2023-12-12 10:03:36 -0300

    mudança de mensagem de erro

--- src/intermediarios/validarParametrosRota.js
+++ src/intermediarios/validarParametrosRota.js
@@ -2,12 +2,9 @@ const validarParametroDeRota = (req, res, next) => {
     const { id } = req.params;
 
     if (id === undefined || isNaN(Number(id))) {
-        return res
-            .status(400)
-            .json({
-                mensagem:
-                    'ID do produto invalido! Insira um numero de ID valido',
-            });
+        return res.status(400).json({
+            mensagem: 'Insira um numero de ID valido',
+        });
     }
     next();
 };
