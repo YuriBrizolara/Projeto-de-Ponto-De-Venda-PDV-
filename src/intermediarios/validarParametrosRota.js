a2a745f2368dc69fcdcf58f8da20d4a48cd6948f
CaioKWY <caiokaway@gmail.com>
2023-12-12 01:08:01 -0300

    incluindo validacao de produto id no intermediario

--- src/intermediarios/validarParametrosRota.js
+++ src/intermediarios/validarParametrosRota.js
@@ -1,14 +1,26 @@
 const validarParametroDeRota = (req, res, next) => {
-    const { id } = req.params;
+    const { id, idProduto } = req.params;
 
-    if (id === undefined || isNaN(Number(id))) {
-        return res
-            .status(400)
-            .json({
-                mensagem:
-                    'ID do produto invalido! Insira um numero de ID valido',
+    if (!id && !idProduto) {
+        return res.status(400).json({
+            mensagem: 'Insira um numero de ID valido'
+        });
+    };
+
+    if (id) {
+        if (id === undefined || isNaN(Number(id))) {
+            return res.status(400).json({
+                mensagem: 'ID do produto invalido! Insira um numero de ID valido'
             });
-    }
+        };
+    };
+    if (idProduto) {
+        if (idProduto === undefined || isNaN(Number(idProduto))) {
+            return res.status(400).json({
+                mensagem: 'ID do produto invalido! Insira um numero de ID valido'
+            });
+        };
+    };
     next();
 };
 
